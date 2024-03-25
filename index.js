29b2d8ac48cdd8637b4eaae53e5050a5f4b547f9
roxannesyombua <roxannesyombua@gmail.com>
2024-03-25 13:23:26 +0300

    update

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let multiply;
 
-// Write your code here
+let num1=2
+let num2=31
+multiply = (num1 * num2)
+
+//
+let random = Math.floor(Math.random() * 10) + 1;
+
+//
+let mod;
+
+let num3=29
+let num4=5
+mod = (num3 % num4)
+
+//
+let max
+
+max=Math.max(12,5,6,14,8,20,3);
+ 
+
+
+
